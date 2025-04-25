01cf4f60b4a34894f436e02abcb91854a771aebf
elif şahin <sahinelif@posta.mu.edu.tr>
2025-04-25 11:59:34 +0300

    Add files via upload

---
+++ week9-elifshn27/src/Rectangle.java
@@ -0,0 +1,37 @@
+public abstarct class Rectangle extends Shape implements Drawable{
+
+    int sideA = 0, sideB = 0;
+
+    public Rectangle(String color, int sideB, int sideA) {
+        super(color);
+        this.sideB = sideB;
+        this.sideA = sideA;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2*(this.sideA + this.sideB);
+    }
+
+    @Override
+    public double area() {
+        return sideA*sideB;
+    }
+}
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Rectangle)) return false;
+        Rectangle rectangle = (Rectangle) obj;
+        return sideA == rectangle.sideA && sideB == rectangle.sideB;
+    }
+
+
+
+
+     @Override
+     public void draw(){
+        System.out.println("A rectangle is drawn");
+     }
+}
---
+++ week9-elifshn27/src/Shape.java
@@ -0,0 +1,19 @@
+public abstract class Shape{
+
+    private String color = "white";
+
+    public Shape(String color){
+        this.color = color;
+
+    }
+    public abstract double perimeter();
+
+    public abstract double area();
+
+    @override
+    public String toString() {
+        return "Shape{" +
+                "color= '"+ color + '\''+
+                '}';
+    }
+}
